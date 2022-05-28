478a17144c0b52f9806bbdd0a150ef9593c49a42
jocamax <jovan.maksin@gmail.com>
2022-05-28 15:02:47 +0200

    Added home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    <main>
+      <div className="main-container">
+        <Link to='/'>
+        <div className="main-box">
+        <img className='main-img' src="https://cdn-icons-png.flaticon.com/512/904/904816.png" alt="" />
+        <p className='home-text-1 tx-large'>Kartice za Menzu</p>
+        </div>
+        </Link>
+        <Link to='/oglasic'>
+        <div className="main-box">
+       <img className='main-img' src="https://cdn-icons-png.flaticon.com/512/578/578110.png" alt="" />
+        <p className='home-text-2 tx-large'>Nadji Cimera</p>
+        </div>
+        </Link>
+   
+      </div>
+    </main>
+
+    </>
   )
 }
 
